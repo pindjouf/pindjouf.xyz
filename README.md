073eb8bd0b5a2c4097226380bd734221e525703a
Esau Bukasa <esaubukasa@pindjouf.home>
2023-11-19 16:03:32 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# pindjouf.xyz
---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gymnasium
+    </title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" type="image/x-icon" href="/assets/3996695.png">
+</head>
+<body>
+    
+</body>
+</html>
+<?php
+echo "Hello!, world";
+?>
