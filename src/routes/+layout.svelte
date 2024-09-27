e989ea6c53c010744dd5a70de0f4f20f2d4de948
Esau <esaubukasa@protonmail.com>
2024-09-27 22:12:14 +0200

    bruh

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,11 @@
 <script>
     import "./style.css";
+    import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+    import { dev } from '$app/environment';
+    import { inject } from '@vercel/analytics';
+
+    inject({ mode: dev ? 'development' : 'production' });
+    injectSpeedInsights();
 </script>
 
 <div class="nav">
@@ -11,3 +17,4 @@
 </div>
 
 <slot></slot>
+
