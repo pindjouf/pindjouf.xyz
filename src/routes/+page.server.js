cccbf4f08228c712ee889078362dec3f52cecf20
Esau <esaubukasa@protonmail.com>
2024-09-29 23:05:28 +0200

    alright

---
+++ src/routes/+page.server.js
@@ -0,0 +1,19 @@
+import { getAllPosts } from '$lib/posts';
+
+export async function load() {
+  const posts = await getAllPosts();
+  
+  // Group posts by year
+  const groupedPosts = posts.reduce((group, post) => {
+    const year = new Date(post.date).getFullYear();
+    if (!group[year]) {
+      group[year] = [];
+    }
+    group[year].push(post);
+    return group;
+  }, {});
+
+  return {
+    groupedPosts
+  };
+}
